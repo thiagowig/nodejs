0ed86836e2edf186778a4a6faffc97ba438d273c
tfonseca <tfonseca@ciandt.com>
2016-07-25 17:48:16 -0300

    Myhouse

--- myhouse/src/index.js
+++ myhouse/src/index.js
@@ -18,21 +18,18 @@ var c = new Crawler({
 c.queue('http://www.uol.com.br');
 */
 
-var request = require("request"),
-  cheerio = require("cheerio"),
-  url = "http://www.uol.com.br";
+var request = require("request");
+var cheerio = require("cheerio");
+var url = 'http://www.vivareal.com.br/venda/minas-gerais/belo-horizonte/bairros/sagrada-familia/apartamento_residencial/#{"precioInicial":"150000","precioFinal":"260000","areaDesde":null,"areaHasta":null,"habitacionesDesde":2,"habitacionesHasta":null,"banosDesde":1,"banosHasta":null,"garajesDesde":1,"garajesHasta":null,"locIds":["BR>Minas Gerais>NULL>Belo Horizonte>Barrios>Sagrada Familia"],"orderBy":null,"orderType":null,"usoProyecto":null,"estadoConstruccionProyecto":null}';
   
 request(url, function (error, response, body) {
   if (!error) {
     var $ = cheerio.load(body);
-    var links = $("a");
+    var articles = $('article');
 
-    console.log('Size: ' + links.length);
-
-    for (var index = 0; index < links.length; index++) {
-        var element = links[index];
-        //console.log(element.attribs.href);
-    }
+    for(var i = 0; i < articles.length; i++) {
+        console.log(articles[i]);
+    }  
       
   } else {
     console.log("We’ve encountered an error: " + error);
