2894167ce9a816896c41049865d1c88417a37a6a
Thiago FOnseca <dev.thiago@gmail.com>
2016-08-19 20:02:41 -0300

    Adding cors

--- ntask-api/libs/middlewares.js
+++ ntask-api/libs/middlewares.js
@@ -1,14 +1,16 @@
 
 var bodyParser = require('body-parser');
 var express = require('express');
+var cors = require('cors');
 
 module.exports = function(app) {
 
     app.set('port', 3000);
     app.set('json spaces', 4);
-
+    
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
+    app.use(express.static('public'));
     
     app.use(function(req, res, next) {
         if (req.body) {
@@ -17,5 +19,9 @@ module.exports = function(app) {
         next();   
     });
 
-    app.use(express.static('public'));
+    app.use(cors({
+        origin: ["http://localhost:300"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowHeaders: ["Content-Type", "Authorization"]
+    }));
 }
