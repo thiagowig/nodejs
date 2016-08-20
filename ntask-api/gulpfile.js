714e2813d8abea4a0d04957530d30cc4d38fb07c
Thiago FOnseca <dev.thiago@gmail.com>
2016-08-20 11:16:49 -0300

    Adding ESLint

--- ntask-api/gulpfile.js
+++ ntask-api/gulpfile.js
@@ -1,8 +1,27 @@
 var gulp = require('gulp');
-var gulpUglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('scripts', function() {
+    console.log('Running my fucking script');
+});
+
+gulp.task('minify', function() {
     gulp.src('*/*.js')    
-      .pipe(gulpUglify())
-      .pipe(gulp.dest('minjs'))
+      .pipe(uglify())
+      .pipe(gulp.dest('minjs'));
+});
+
+gulp.task('default', ['scripts', 'minify']);
+
+
+gulp.task('watch', function() {
+    gulp.watch('*/*.js', ['scripts']);
+});
+
+gulp.task('lint', function() {
+    return gulp.src('routes/indexRoute.js')    
+               .pipe(eslint())
+               .pipe(eslint.format())
+               .pipe(eslint.failOnError());
 });
--- ntask-api/routes/indexRoute.js
+++ ntask-api/routes/indexRoute.js
@@ -10,8 +10,7 @@ module.exports = function(app) {
      *      HTTP/1.1 200 OK
      *      {status: 'NTask API'}
      */
-    app.get('/', function(req, res) {
-        res.json({status: 'NTask API'});
-    }); 
-    
-}
+app.get('/', function(req, res) {
+    res.json({status: 'NTask API'});
+}); 
+};
