7cb7e8a5d772acc3d80b51178cca87e7c59b965a
tfonseca <tfonseca@ciandt.com>
2016-09-19 19:21:44 -0300

    Async neles

--- async-test/index.js
+++ async-test/index.js
@@ -1,11 +1,13 @@
 
 
 var async = require('async');
+
+/*
 var fs = require('fs');
 
-var dirs = ["/home/thiago/dev/repo", "/home/thiago/dev/repo/mycar"];
+var dirs = ["/Users/tfonseca/dev/repo/nodejs", "/Users/tfonseca/dev/repo/nodejs/tutorial"];
 
-async.concat(dirs, fs.readdir, function(err, files) {
+async.concatSeries(dirs, fs.readdir, function(err, files) {
     if(err) {
         console.log("\n\n\nERROR");
         console.log(err);
@@ -14,3 +16,117 @@ async.concat(dirs, fs.readdir, function(err, files) {
         console.log(files);
     }
 })
+*/
+
+/*
+async.parallel([
+    function() {
+        setTimeout(function() {
+            console.log("First function");
+        }, 1000);
+    },
+    function() {
+        console.log("Second function");
+    }
+
+], function(err, results) {
+    console.log(err);
+});
+*/
+/*
+async.parallelLimit([
+    function() {
+            console.log("First function");
+    },
+    function() {
+        console.log("Second function");
+    },
+    function() {
+        console.log("Third function");
+    },
+    function() {
+        console.log("Fourth function");
+    },
+    function() {
+        console.log("Fifth function");
+    },
+    function() {
+        console.log("Sixth function");
+    },
+    function() {
+        console.log("Seventh function");
+    },
+    function() {
+        console.log("Eighth function");
+    },
+    function() {
+        console.log("Ninth function");
+    },
+    function() {
+        console.log("Tenth function");
+    }
+
+], 8, function(err, results) {
+    console.log(err);
+});
+*/
+
+/*
+var myMethod = function(err, result) {
+    console.log(myWrongObject.throwAnException);
+    return "Thiago";
+}
+
+async.retry({
+    times: 3, 
+    interval: 1000
+}, myMethod, function(err, result) {
+    if (err) {
+        console.log('Error in retry');
+    } else {
+        console.log('Sucess in retry: ' + result);
+    }
+});*/
+
+/*
+async.series([
+    function (callback) {
+        console.log('Running one');
+        callback(null, 'one');
+    },
+    function (callback) {
+        console.log('Running two');
+        callback(null, 'two');
+    }
+], function (err, results) {
+    console.log(results);
+});
+*/
+
+/*
+var createUser = function(id, callback) {
+    callback(null, {
+        id: 'user' + id
+    });
+};
+
+async.times(15, function(n, next) {
+    createUser(n, function(err, user) {
+        next(err, user);
+    });
+}, function(err, users) {
+    console.log(users);
+});
+
+*/
+
+var count = 0;
+
+async.until(function() {
+    return count > 3;
+}, function(err) {
+    console.log(count);
+    count++;
+});
+
+
