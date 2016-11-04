dddecfffc25ccdc8c3cf5ab08de21b716c23e472
tfonseca <tfonseca@ciandt.com>
2016-11-04 19:33:40 -0200

    News

--- sequelize-test/index.js
+++ sequelize-test/index.js
@@ -2,16 +2,30 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('postgres://postgres:pass@localhost:5432/gestao_cervejeira');
 
 const Cerveja = connection.define("cerveja",  {
-  nome: Sequelize.STRING,
-  graduacaoAlcoolica: Sequelize.INTEGER
+  nome: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  graduacaoAlcoolica: Sequelize.INTEGER,
+  descricao: {
+    type: Sequelize.STRING,
+    defaultValue: 'Coming soon...'
+  }
 });
 
 connection.sync({
+  force: true,
   logging: console.log
   
 }).then(function() {
   Cerveja.create({
     nome: "Colorado Indica",
     graduacaoAlcoolica: 8
+  }).then(function() {
+    Cerveja.findAll().then(function(result) {
+      console.log("##### " + result[0].nome);
+    })
   });
+}).catch(function(error) {
+  console.log(error);
 });
