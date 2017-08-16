c51b81f3aa498234f96e885d9d2eb02df37c5fb0
Thiago Fonseca <dev.thiago@gmail.com>
2017-08-16 19:09:14 -0300

    Including the syntax editor

--- json-schema-validator/public/schema.js
+++ json-schema-validator/public/schema.js
@@ -1,14 +1,37 @@
-$(document).ready(function() {
-    $('#selectedSchema').change(function(varValue) {
+$(document).ready(function () {
+    var aceEditor = window.ace.edit("editor");
+
+    aceEditor.setOptions({
+        showPrintMargin: false, // hides the vertical limiting strip
+        maxLines: 20,
+        minLines: 20,
+        fontSize: "100%" // ensures that the editor fits in the environment
+    });
+    // defines the style of the editor
+    aceEditor.setTheme("ace/theme/twilight");
+    // hides line numbers (widens the area occupied by error and warning messages)
+    aceEditor.renderer.setOption("showLineNumbers", true);
+    // ensures proper autocomplete, validation and highlighting of JavaScript code
+    aceEditor.getSession().setMode("ace/mode/json");
+
+
+    $('#selectedSchema').change(function (varValue) {
         var fileName = $('#selectedSchema').find(":selected").val();
 
-        $.ajax({
-            url: '/schema/' + fileName
-        }).done(function(content, status, response) {
-            content = JSON.parse(content).replace('\n', '&#010;')
-            $('#my-code-wrapper').html(content)
-        }).fail(function(content, status, response) {
-            alert('Error: ' + content)
-        })
+        if (fileName) {
+            $.ajax({
+                url: '/schema/' + fileName
+            }).done(function (content, status, response) {
+                content = JSON.parse(content).replace('\n', '')
+                //$('#editor').val(content)
+                aceEditor.setValue(content, -1)
+            }).fail(function (content, status, response) {
+                alert('Error: ' + content)
+            })
+
+        } else {
+            aceEditor.setValue('', -1)
+        }
+
     })
 })
