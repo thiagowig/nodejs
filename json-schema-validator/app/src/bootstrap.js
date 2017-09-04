547f4744938434ce49a228c11026b75ff581dbd1
Thiago Fonseca <dev.thiago@gmail.com>
2017-09-04 11:00:06 -0300

    Adding security

--- json-schema-validator/app/src/bootstrap.js
+++ json-schema-validator/app/src/bootstrap.js
@@ -4,16 +4,25 @@ module.exports.init = function () {
     var app = express()
     var bodyParser = require('body-parser')
     var consign  = require('consign')
+    var helmet = require('helmet')
+    var compression = require('compression')
+    var cors = require('cors')
+    var http = require('http')
 
     app.set('view engine', 'ejs')
     app.set('views', viewsDir);
     app.use(express.static(staticDir));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(helmet())
+    app.use(compression())
+    app.use(cors())
 
-    consign({verbose: false}).include('app/src/controller').into(app)
+    consign({verbose: false})
+        .include('app/src/controller')
+        .into(app)
 
-    app.listen(8080)
-    console.log('8080 is the magic port');
+    http.createServer(app).listen(8080)
+    console.log('Server is up and running!!!');
 }
 
